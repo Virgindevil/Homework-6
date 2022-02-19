a4f70e4742ea2d323cbffd281572413e82675705
S.Matuzov <98310589+Virgindevil@users.noreply.github.com>
2022-02-20 00:01:41 +0400

    Add files via upload

--- Homework 6/Program.cs
+++ Homework 6/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,136 +9,12 @@ namespace Homework_6
 {
     class Program
     {
-        
         static void Main(string[] args)        
-        {            
-
+        {
+            string path = "savefile.txt";
+            Repository rep = new Repository(path);
             Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Menu();
-        }
-
-        static void ReadFile()
-        {
-            Console.Clear();
-            string path = "db.txt";
-            if (File.Exists(path) == true)
-            {
-                using (StreamReader sr = new StreamReader(path, Encoding.Unicode))
-                {
-                    string line;
-
-                    Console.WriteLine($"{"ID"} {" Время",15} {" ФИО",20} {" Возраст",20} {"\tРост"} {"\tДата рождения",5} {"\tМесто рождения",5}");
-                    Console.WriteLine();
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        string[] data = line.Split('#');
-                        Console.WriteLine($" {data[0]}" +
-                                          $"  {data[1]}" +
-                                          $"  {data[2]}" +
-                                          $"\t{data[3]}" +
-                                          $"\t{data[4]}" +
-                                          $"\t{data[5]}" +
-                                          $"\t{data[6]}");
-                    }
-                }
-            }
-            else
-            {
-                Console.WriteLine($"\nФайл ещё не был создан...");
-            }
-            
-
-            Console.WriteLine($"\n\n\nДля выхода в меню нажмите любую клавишу...");
-            Console.ReadKey();
-        }
-
-        static void WriteFile()
-        {
-            Console.Clear();
-
-            string path = "db.txt";
-            int id = NumberId(path);
-            using (StreamWriter sw = new StreamWriter(path, true, Encoding.Unicode))
-            {
-                char key = 'д';
-
-                do
-                {
-                    Console.Clear();
-
-                    string note = string.Empty;
-                    note += $"{id}#";
-                    id++;
-
-                    string now = DateTime.Now.ToString();
-                    note += $"{now}#";
-
-                    Console.Write("Введите фамилию имя отчество: ");
-                    note += $"{Console.ReadLine()}#";
-
-                    Console.Write("Введите возраст: ");
-                    note += $"{Console.ReadLine()}#";
-
-                    Console.Write("Введите рост: ");
-                    note += $"{Console.ReadLine()}#";
-
-                    Console.Write("Введите дату рождения: ");
-                    note += $"{Console.ReadLine()}#";
-
-                    Console.Write("Введите место рождения: ");
-                    note += $"{Console.ReadLine()}";
-
-                    sw.WriteLine(note);
-
-                    Console.Write("Продожить н/д"); key = Console.ReadKey(true).KeyChar;
-
-                } while (char.ToLower(key) == 'д');
-            }
-        }
-
-        static void Menu()
-        {
-            while (true)
-            {
-                Console.Clear();
-                Console.WriteLine($"\t\t\t\t\t1 — вывести данные на экран. ");
-                Console.WriteLine("\t\t\t2 — заполнить данные и добавить новую запись в конец файла. ");
-                Console.WriteLine("\t\t\t\t\t\t0 - выход.");
-                Console.Write("\n\t\t\t\t\t\t");
-                switch (Console.ReadLine())
-                {
-                    case "1":
-                        ReadFile();
-                        break;
-
-                    case "2":
-                        WriteFile();
-                        break;
-
-                    case "0":
-                        return;
-
-                    default:
-                        break;
-                }
-            }
-        }
-
-        static int NumberId(string path)
-        {
-            int id = 1;
-            if (File.Exists(path) == true)
-            {
-                using (StreamReader sr = new StreamReader(path, Encoding.Unicode))
-                {
-                    string line;
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        id++;
-                    }
-                }
-            }
-            return id;
+            rep.Menu();            
         }
     }
 }
